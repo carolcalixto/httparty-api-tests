b0815247ccd3e2579f4e73ff3afb26ac9f040f8e
Caroline Calixto <carollinecalixto@gmail.com>
2020-01-06 15:17:03 -0300

    implemented new test for wrong user email

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -15,4 +15,20 @@ describe "post" do
         it { expect(@result.response.code).to eql "200" }
     end
 
+    context "when wrong email" do
+        before do
+            @new_user = build(:user_wrong_email).to_hash
+
+            @result = HTTParty.post(
+                "http://localhost:3001/user",
+                body: @new_user.to_json,
+                headers: {
+                    "Content-Type" => "application/json"
+                }
+            )
+        end
+        it { expect(@result.response.code).to eql "412" }
+        it { expect(@result.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
+    end
+    
 end
