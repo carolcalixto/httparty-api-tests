c04df9567017d3b6dd2baaaf0d6d28fd0b74743b
Caroline Calixto <carollinecalixto@gmail.com>
2020-01-06 11:42:05 -0300

    refactoring the httparty tests wih context

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,18 +1,19 @@
+describe "post" do
+    context "when new user" do
+        before do
+            @new_user = { full_name: "Caroline Calixto", email: "ccalixto@gmail.com", password: "pwd123" }
+            Database.new.delete_user(@new_user[:email])
 
+            @result = HTTParty.post(
+                "http://localhost:3001/user",
+                body: @new_user.to_json,
+                headers: {
+                    "Content-Type" => "application/json"
+                }
+            )
+        end
 
-describe "cadastro" do
-    
-    it "novo usuário" do
-        Database.new.delete_user("falecomcarolcalixto@gmail.com")
+        it { expect(@result.response.code).to eql "200" }
 
-        result = HTTParty.post(
-            "http://localhost:3001/user", 
-            body: {full_name: "Carol Calixto", email: "falecomcarolcalixto@gmail.com", password: "pwd123"}.to_json,
-            headers: {
-                "Content-Type" => "application/json"
-            }
-        )
-        puts result
-        expect(result.response.code).to eql "200"
     end
 end
