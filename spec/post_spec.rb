5c99e01901b1a39f6bb6c6b71eec12339d60dddb
Caroline Calixto <carollinecalixto@gmail.com>
2020-01-06 15:25:15 -0300

    added new abstraction layer - user

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,34 +1,20 @@
+
+
 describe "post" do
     context "when new user" do
         before do
             @new_user = build(:user).to_hash
-
-            @result = HTTParty.post(
-                "http://localhost:3001/user",
-                body: @new_user.to_json,
-                headers: {
-                    "Content-Type" => "application/json"
-                }
-            )
+            @result = ApiUser.save(@new_user)
         end
-
         it { expect(@result.response.code).to eql "200" }
     end
 
     context "when wrong email" do
         before do
             @new_user = build(:user_wrong_email).to_hash
-
-            @result = HTTParty.post(
-                "http://localhost:3001/user",
-                body: @new_user.to_json,
-                headers: {
-                    "Content-Type" => "application/json"
-                }
-            )
+            @result = ApiUser.save(@new_user)
         end
         it { expect(@result.response.code).to eql "412" }
         it { expect(@result.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
     end
-    
 end
